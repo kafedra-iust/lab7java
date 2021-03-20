6ad70ea4d4fecf722b9975d0dade31dc218831bd
kafedra-iust <kafedra.iust@gmail.com>
2021-03-20 21:55:09 +0200

    Added info about ArrayList

--- src/main/java/demo/Main.java
+++ src/main/java/demo/Main.java
@@ -1,7 +1,25 @@
 package demo;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.not;
+
 public class Main {
     public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        for (int i=0; i<16; i++) {
+            list.add(Integer.toHexString(i));
+        }
+        List<String> stringsToSearch = new ArrayList<>(list);
+        stringsToSearch.addAll(list); // додаємо рядки ще раз
 
+        List<String> copy = new ArrayList<>(stringsToSearch);
+        Collections.sort(copy);
+        int index = Collections.binarySearch(copy, "f");
+        assert(index > 0);
     }
 }
